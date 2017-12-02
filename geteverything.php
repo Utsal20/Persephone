de77ac944cc0379f2e45fe3d3ceeb7c90ee2b0f2
Utsal Shrestha <utsal.shres@gmail.com>
2017-12-01 23:08:27 -0800

    a

--- geteverything.php
+++ geteverything.php
@@ -4,21 +4,21 @@
   //connection settings
   $connectionInfo = array("UID" => "srajah19", "pwd" => "Peteisnice2017", "Database" => "LinkedUs")
   $serverName = "linkedus.database.windows.net";
-  $conn = sqlsrv_connect($serverName, $connectionInfo);
+  $conn = sqlsrv_connect($serverName, $connectionInfo) or die("<qryn>".print_r(sqlsrv_errors(), TRUE)."</qryn>";;
   
   //Query to get data from Person table
   $sql1 = "SELECT * FROM Person WHERE url = ".$url;
   $going = sqlsrv_query($conn, $sql1) or die("<p>".print_r(sqlsrv_errors(), TRUE)."</p>";
   $row1 = sqlsrv_fetch_array($going);
   $pid = $row['Pid'];
-
+  
+  /*
   //Remaining queries
   $sql2 = "SELECT * FROM Experience WHERE Pid = $pid";
   $sql3 = "SELECT * FROM Projects WHERE Pid = $pid";
   $sql4 = "SELECT * FROM Skills WHERE Pid = $pid";
-  
+  */
   echo $pid;
-  echo $row1;
 
   echo $url;
 ?>
