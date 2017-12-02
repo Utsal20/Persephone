a510b8035d4e7053525cff57e510f8d9b306109f
Utsal Shrestha <utsal.shres@gmail.com>
2017-12-02 13:16:40 -0800

    post set up

--- writingtodatabase.php
+++ writingtodatabase.php
@@ -1,1 +1,18 @@
-<
+<?php
+  $name = $_POST["name"];
+  $email = $_POST["email"];
+  $phone = $_POST["phonenumber"];
+  $address = $_POST["address"];
+  $github = $_POST["github"];
+
+  $etitle = $_POST["Etitle"];
+  $ecompany = $_POST["Ecompany"];
+  $edate = $_POST["Edate"];
+  $edescription = $_POST["Edescription"];
+
+  $ptitle = $_POST["Ptitle"];
+  $plink = $_POST["Plink"];
+  $pdescription = $_POST["Pdescription"];
+
+  $skill = $_POST["skill"];
+?>
