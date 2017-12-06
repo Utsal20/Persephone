6d94ca73fa0cab0291161912901bc8ff5de3cb71
Utsal Shrestha <utsal.shres@gmail.com>
2017-12-06 15:15:40 -0800

    basic info completed

--- geteverything.php
+++ geteverything.php
@@ -21,13 +21,20 @@
     echo '<body><div class="nameDiv"><h1>'.$row1['Name'].'</h1>';
     echo '<div class="gitHubBubbleContainer"><a href="'.$row1['Github'].'" target="blank"><div class="gitHubBubble"></div></a></div>';
     echo '<div class="emailBubbleContainer"><a href="mailto:'.$row1['Email'].'"><div class="emailBubble"></div></a></div>';
+    
+    echo '<div class="basicInfoDiv"><div class="phoneNumberDiv"><h2 class="textAlignRight">Phone #</h2>';
+    echo '<h3 class="textAlignRight">'.$row1['Phone'].'</h3></div>';
+    echo '<div class="addressDiv"><h2 class="textAlignLeft">Address</h2><h3 class="textAlignLeft">'.$row1['Address'].'</h3></div></div></div>';
   }
 
   //Remaining queries</a></div>
   $sql2 = "SELECT * FROM Experience WHERE Pid = ".$pid;
+  $going = sqlsrv_query($conn, $sql2) or die("<p>".print_r(sqlsrv_errors(), TRUE)."</p>");
+  while ($row2 = sqlsrv_fetch_array($going)){
+    echo 'Success';
+  }
+
   $sql3 = "SELECT * FROM Projects WHERE Pid = ".$pid;
   $sql4 = "SELECT * FROM Skills WHERE Pid = ".$pid;
-  
-  echo $pid."<br>";
-  echo $url."c";
+
 ?>
