ffd7aa8dee811d5e80e03941a4ec90a7b4fd5bbf
Utsal Shrestha <utsal.shres@gmail.com>
2017-12-08 10:19:04 -0800

    its a trap!

--- writingtodatabase.php
+++ writingtodatabase.php
@@ -5,17 +5,22 @@
   $address = $_POST["address"];
   $github = $_POST["github"];
 
-  if ($name == ""  || $email=="" || $phone=="" || $address=="" || $github=="")
-    echo "One or more required fields left empty. Please go back and fill them up.<br>";
+  if ($name==""  || $email=="" || $phone=="" || $address=="" || $github=="")
+    die("One or more required fields left empty. Please go back and fill them up.<br>");
 
   $etitle = $_POST["Etitle"];
   $ecompany = $_POST["Ecompany"];
   $edate = $_POST["Edate"];
   $edescription = $_POST["Edescription"];
 
+  if (($etitle=="" || $ecompany=="") && ($edate != "" || $edescription != ""))
+    die("One or more required fields left empty. Please go back and fill them up.<br>");
+
   $ptitle = $_POST["Ptitle"];
   $plink = $_POST["Plink"];
   $pdescription = $_POST["Pdescription"];
+  if (($ptitle =="") && ($pdescription != "" || $plink != ""))
+    die("One or more required fields left empty. Please go back and fill them up.<br>");
 
   $skill = $_POST["skill"];
 ?>
